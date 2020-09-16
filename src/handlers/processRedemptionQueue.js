e59267711306a43ee8d1995eb50694e23de922c9
niranjanbhambi <niranjan.bhambi@suretekinfosoft.com>
2020-09-16 17:58:41 +0530

    Some more changes

--- src/handlers/processRedemptionQueue.js
+++ src/handlers/processRedemptionQueue.js
@@ -1,13 +1,13 @@
-import createError from "http-errors";
+// import createError from "http-errors";
 import { sendEmailToAdmin } from "./sendMail";
 import axios from "axios";
 import { API_End_Point, processTransaction } from "../lib/utils";
 
 async function processRedemptionQueue(event, context) {
-  const record = event.Records[0];
-  const { user_id, item_id, amount } = JSON.parse(record.body);
-
   try {
+    const record = event.Records[0];
+    const { user_id, item_id, amount } = await JSON.parse(record.body);
+
     // fetch item info via api
     const item = await axios.get(`${API_End_Point}/getItemsById/${item_id}`);
 
@@ -23,11 +23,10 @@ async function processRedemptionQueue(event, context) {
         const body = `Hello Admin, Item Id: ${item_id} is no longer available please, initiate refund for UserId: ${user_id} Amount deducted :${amount}.`;
         const subject = `Initiate Refund UserId: ${user_id}, ItemId: ${item_id}, Amount: ${amount}`;
         await sendEmailToAdmin(subject, body);
-        throw new createError.Forbidden("No Inventory");
+        // throw new createError.Forbidden("No Inventory");
       }
     } else {
       console.log("Item does not exist");
-      throw new createError.NotFound("Item not found");
     }
   } catch (error) {
     console.log("Error fetching api: ", error);
